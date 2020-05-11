ef256fb36b8e3d94b713c04deb44fce1da0fe6fe
FadedHearts <tlfoot@hotmail.com>
2020-05-11 04:07:59 +0000

    project test fav post

--- main.py
+++ main.py
@@ -98,12 +98,20 @@ def get_favorites():
     favs = [fav.toDict() for fav in favs]
     return json.dumps(favs)
 
-@app.route('/favorites', methods=['GET'])
+@app.route('/favorites/<instid>', methods=['POST'])
 @login_required
-def get_favorites():
-    favs = Favorite.query.filter_by(userid=current_user.id).all()
-    favs = [fav.toDict() for fav in favs]
-    return json.dumps(favs)
+def add_favorite(instid):
+    data = request.get_json()
+    inst = Instructor.query.get(instid)
+    if inst == None:
+        return 'Invalid instructor id or unauthorized', 403
+    fav = Favorite.query.get((current_user.id, instid))
+    if fav != None:
+        return 'Instructor already a favorite', 403
+    data = Favorite(userid=current_user.id, instructorid=instid)
+    db.session.add(data)
+    db.session.commit()
+    return "Instructor added to favorites", 201
 
 @app.route('/favorites/<instid>', methods=['POST'])
 @login_required
